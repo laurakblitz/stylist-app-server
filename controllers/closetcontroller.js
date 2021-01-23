dc6f73acc65fb853b82f1990fc852402e7a3b52f
Ivan Artemiev <29709626+iartemiev@users.noreply.github.com>
2021-01-23 15:40:19 -0500

    /allcloset -> /allclosetposts

--- controllers/closetcontroller.js
+++ controllers/closetcontroller.js
@@ -42,7 +42,7 @@ router.post('/upload', validateSession, upload.single('image'), (req, res) => {
 });
 
 // ******************** (GET) Get all closet posts ******************** //
-router.get('/allcloset', validateSession, (req, res) => {
+router.get('/allclosetposts', validateSession, (req, res) => {
 
     Closet.findAll({
         where: { userId: req.user.id }
