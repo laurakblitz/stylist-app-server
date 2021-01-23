2775d7f9fed9067892c1a707def7b51c399872c2
Laura Blitz <laura.k.blitz@gmail.com>
2021-01-23 13:38:58 -0500

    Update

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const User = require('../models/user');
-const validateSession = require('../middleware/validateSession');
+const validateSession = require('../middleware/validateSession.js');
 
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
